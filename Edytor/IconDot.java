845dbb3922f72fc564859a310a6b975eec15463d
Ewelina <pd3531@pjwstk.edu.pl>
2022-04-20 14:51:32 +0200

    edytor1

---
+++ Edytor/IconDot.java
@@ -0,0 +1,34 @@
+package Edytor;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import javax.swing.Icon;
+
+
+public class IconDot implements Icon {
+	
+	  private Color color;
+	 
+	  IconDot(Color c) {
+		    color = c;  }
+
+
+	  public void paintIcon(Component c, Graphics g, int x, int y) {
+		    g.setColor(color);
+		    g.fillOval(5, 5, 10, 10);
+
+		    }
+
+	@Override
+	public int getIconWidth() {
+		return 10;
+	}
+
+	@Override
+	public int getIconHeight() {
+		return 10;
+	}
+	
+
+}
---
+++ Edytor/IconLine.java
@@ -0,0 +1,32 @@
+package Edytor;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import javax.swing.Icon;
+
+
+public class IconLine implements Icon {
+
+	private Color color;
+
+	IconLine(Color c) {
+		color = c;
+	}
+
+	public void paintIcon(Component c, Graphics g, int x, int y) {
+		g.setColor(color);
+		g.drawLine(5, 6, 100, 6);
+	}
+
+	@Override
+	public int getIconWidth() {
+		return 0;
+	}
+
+	@Override
+	public int getIconHeight() {
+		return 0;
+	}
+
+}
